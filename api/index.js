340c72f1bc4c0333787a03c369dc05759f70520a
Daniel Matheus <danielmcrocha2@gmail.com>
2022-02-16 19:52:48 -0300

    Removendo erro da API usuarios

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 
-const usuariosRouter = require('./usuarios')
 const imoveisRouter = require('./imoveis')
 
 const router = express.Router()
@@ -9,7 +8,6 @@ router.get('/', (req, res) => {
   res.send('App Online!')
 })
 
-router.use('/usuarios', usuariosRouter)
 router.use('/imoveis', imoveisRouter)
 
 module.exports = router
--- models/index.js
+++ models/index.js
@@ -2,14 +2,11 @@ const sequelize = require('../config/sequelize')
 const Sequelize = require('sequelize')
 
 const Imovel = require('./imovel')
-const Usuario = require('./usuario')
 
 const imovel = Imovel(sequelize, Sequelize.DataTypes)
-const usuario = Usuario(sequelize, Sequelize.DataTypes)
 
 const db = {
   imovel,
-  usuario,
   sequelize
 }
 
